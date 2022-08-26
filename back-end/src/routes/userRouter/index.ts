dc3b90b9f49ceb53d20ac15cf9f6bbd6fd39dd34
João Victor Arouca <joao.silveira@estudante.ufla.br>
2022-08-26 18:20:57 -0300

    feat: create routes user

---
+++ back-end/src/routes/userRouter/index.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+// import UserController from "../../controller/user-controller";
+// import UserService from "../../service/user-service";
+// import Validator from "../../tools/validator";
+
+const userRouter = Router();
+
+// const validator = new Validator()
+// const userService = new UserService(validator)
+// const user = new UserController(userService);
+
+userRouter.post('/login');
+userRouter.post('/register');
+
+export default userRouter;
