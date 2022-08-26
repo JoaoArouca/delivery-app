7d5e512d4bec8ed613dacff2e3365f4ae869cfd6
João Victor Arouca <joao.silveira@estudante.ufla.br>
2022-08-26 18:53:01 -0300

    fix(issues): im gonna learn prisma and update my app

--- back-end/src/routes/userRouter/index.ts
+++ back-end/src/routes/userRouter/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 // import UserController from "../../controller/user-controller";
 // import UserService from "../../service/user-service";
-// import Validator from "../../tools/validator";
+// import Validator from '../../tools/Validator';
 
 const userRouter = Router();
 
