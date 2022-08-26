94399c2381be1540d1b7016aacf5e3ba439c0e31
João Victor Arouca <joao.silveira@estudante.ufla.br>
2022-08-26 18:25:51 -0300

    feat: create service user interface

--- back-end/src/interfaces/index.ts
+++ back-end/src/interfaces/index.ts
@@ -4,3 +4,17 @@ export interface IUser {
   email: string,
   password: string
 }
+
+export interface IUserService {
+  // login({
+  //   email,
+  //   password,
+  // }: Partial<Pick<IUser, 'email' | 'password'>>): Promise<
+  //   Omit<IUser, 'password'>
+  // >;
+  register({
+    name,
+    email,
+    password,
+  }: Partial<Omit<IUser, 'id'>>): Promise<Omit<IUser, 'password'>>;
+}
