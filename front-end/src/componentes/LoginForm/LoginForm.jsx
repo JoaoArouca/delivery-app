5e2ddd963d79b5e24d616599133a7e53f113da1d
João Victor <joao.silveira@estudante.ufla.br>
2022-09-04 21:06:45 -0300

    feat: enable button when forms are filled

--- front-end/src/componentes/LoginForm/LoginForm.jsx
+++ front-end/src/componentes/LoginForm/LoginForm.jsx
@@ -1,17 +1,23 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import Button from 'react-bootstrap/Button';
 
 export default function LoginForm() {
   const {
-    register, handleSubmit, formState: { errors, isValid, isDirty },
-  } = useForm();
+    register, handleSubmit, formState: { errors, isValid },
+  } = useForm({
+    reValidateMode: 'onChange',
+    mode: 'onChange',
+  });
+
   const onSubmit = (data) => console.log(data);
 
   return (
     <form>
-      <input placeholder="Email" {...register('email', { required: true })} />
+      <input
+        placeholder="Email"
+        {...register('email', { required: true })}
+      />
 
       <input
         placeholder="Senha"
@@ -23,13 +29,11 @@ export default function LoginForm() {
       {errors.password && errors.password.type === 'required' && <span>Password required</span>}
       {errors.password && errors.password.type === 'minLength' && <span>ate least 6 characters</span>}
 
-      <Button
-        disabled={!isDirty || !isValid}
-        type="button"
+      <input
+        disabled={!isValid}
+        type="submit"
         onClick={handleSubmit(onSubmit)}
-      >
-        Entrar
-      </Button>
+      />
     </form>
   );
 }
